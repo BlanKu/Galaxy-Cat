25b948e33938ae258e535150eca056b363885d57
Jakub Rosiński <jakub.rosinski2k4@gmail.com>
2024-10-11 16:34:02 +0200

    Zmiany kosmetyczne
    Dodanie paru textur, skryptów, poprawa tła, utrudnienie gry.

---
+++ Assets/Scripts/backgroundScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundScript : MonoBehaviour
+{
+    public float speed;
+    public float y_multiply;
+
+    Rigidbody2D _rigidbody2D;
+    Transform _transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>(); 
+        _transform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _rigidbody2D.velocity = new Vector3(-speed, -speed*y_multiply, 3);
+        if(_transform.position.x <= -6)
+        {
+            _transform.position = new Vector3(0, 3, 3);
+        }
+    }
+}
